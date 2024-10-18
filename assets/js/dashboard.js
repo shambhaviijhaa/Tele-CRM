e35c83bd74f42b51d2f1ffac4f9383fe4378a371
shambhaviijhaa <shambhavi.jha543@gmail.com>
2024-10-18 15:18:45 +0530

    updated sub-menu

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,9 +1,15 @@
-// Function to show different sections on click and highlight selected item
+// Function to show different sections on click and close open submenus
 function showSection(sectionId, menuItem) {
+    // Close any open submenus
+    closeAllSubmenus();
+
+    // Hide all other sections
     var sections = document.getElementsByClassName("content");
     for (var i = 0; i < sections.length; i++) {
         sections[i].style.display = "none";
     }
+
+    // Show the selected section
     document.getElementById(sectionId).style.display = "block";
 
     // Remove 'selected' class from all main menu items
@@ -11,16 +17,30 @@ function showSection(sectionId, menuItem) {
     for (var i = 0; i < menuItems.length; i++) {
         menuItems[i].classList.remove("selected");
     }
+
     // Add 'selected' class to the clicked main menu item
     if (menuItem) {
         menuItem.classList.add("selected");
     }
 }
 
-// Function to toggle the visibility of the "Add Lead" submenu
+// Function to toggle the visibility of the "Add Lead" submenu and close others
 function toggleSubsection(submenuId) {
     var submenu = document.getElementById(submenuId);
+
+    // Close other open submenus
+    closeAllSubmenus();
+
+    // Toggle the selected submenu visibility
     submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+}
+
+// Function to close all submenus
+function closeAllSubmenus() {
+    var submenus = document.getElementsByClassName("sub-menu");
+    for (var i = 0; i < submenus.length; i++) {
+        submenus[i].style.display = "none";
+    }
 }
 
 // Function to show specific subsection content for "Add Lead" and highlight submenu
@@ -38,26 +58,24 @@ function showSubContent(contentId, submenuItem) {
     showSection('add-lead');
 
     // Remove 'selected' class from all submenu items
-    var submenuItems = document.getElementsByClassName("sub-menu")[0].getElementsByTagName("li");
+    var submenuItems = submenuItem.parentElement.getElementsByTagName("li");
     for (var i = 0; i < submenuItems.length; i++) {
         submenuItems[i].classList.remove("selected");
     }
-    
+
     // Add 'selected' class to the clicked submenu item
     if (submenuItem) {
         submenuItem.classList.add("selected");
     }
 }
-// Placeholder Gmail and Google Drive integration functions
-function integrateGmail() {
-    alert("Gmail integration coming soon!");
-}
 
-function integrateGoogleDrive() {
-    alert("Google Drive integration coming soon!");
-}
+// Detect clicks outside the submenus and close them
+document.addEventListener('click', function(event) {
+    // Check if the click is outside any sub-menu or icon-container
+    var isClickInsideSidebar = event.target.closest('.icon-container, .sub-menu');
 
-// Placeholder search function
-function searchLeads(value) {
-    console.log("Searching leads for: " + value);
-}
+    if (!isClickInsideSidebar) {
+        // Close all submenus if the click was outside
+        closeAllSubmenus();
+    }
+});
